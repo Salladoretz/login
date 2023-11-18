1b00b9dcf1fa93d55b57660e1b5c2dd4559259f9
Salladoretz <salladoretz@yandex.ru>
2023-11-18 16:19:46 +0300

    fix router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './components/App/App'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
+    <HashRouter>
+        <App/>
+    </HashRouter>
 )
